2633905af469f59a0808f80fdb04c7322a946188
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-04-04 20:23:24 +0530

    array revision

--- array.java
+++ array.java
@@ -1,3 +1,11 @@
 public class array {
-    
+    public static void main(String[] args) {
+    int arr[]; // declare
+    arr = new int[3]; // memory allocation
+    arr[0] = 10;
+    int a[]=new int[3];
+    int[] b = new int[3];
+    System.out.println(arr);
+    System.out.println(b);
+    }
 }
