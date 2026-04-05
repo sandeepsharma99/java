9ed17b55fe875f5e118fc8fc9426576935662765
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-04-05 19:48:49 +0530

    revision

--- array.java
+++ array.java
@@ -10,7 +10,7 @@ public class array {
     // arr[0] = 10;
     // int a[]=new int[3];
     // int[] b = new int[3];
-     // int[] c = {1,2,3,4,5,6,7,8,9};
+     // int[] c = {1,2,3,4,5,6,7,8,9}; //creating array with value
     // System.out.println(arr);
     // System.out.println(a);
     // System.out.println(b);
@@ -29,17 +29,33 @@ public class array {
     // Taking input from user
     System.out.println("enter the length of array");
     int n = sc.nextInt();
-    int[] arr = new int[n];
+    int[] arr = new int[n]; //declaring and creaing array
     System.out.println("enter array item");
+    // Taking input using loop
     for(int i=0; i<arr.length; i++){
        int a = sc.nextInt();
         arr[i] = a;
     }
     System.out.println("Given array");
+    // looping on array
     for (int i : arr) {
         System.out.print(i+" ");
     }
+    int ans = sum_arr(arr);
+    System.out.println(ans);
 
+    sc.close();
 
     }
+
+    public static int sum_arr(int[] arr) {
+        int ans = 0;
+        for(int i: arr){
+            ans += i;
+        }
+        return ans;
+    }
 }
+
+
+
