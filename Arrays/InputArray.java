f16d465fcb9ebe7937b7a4c2c010b06914c6c887
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-04-08 21:38:48 +0530

    revision

--- Arrays/InputArray.java
+++ Arrays/InputArray.java
@@ -4,19 +4,33 @@ public class InputArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // case -> 1 given n element
-        int n = sc.nextInt(); // taking size as an input
-        int[] arr = new int[n]; // declaration
+        // int n = sc.nextInt(); // taking size as an input
+        // int[] arr = new int[n]; // declaration
         
-        // Taking input array element from user 
-        for(int i=0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
+        // inserting element 
+        // for(int i=0; i<n; i++){
+        //     arr[i] = sc.nextInt(); //Taking input array element from user 
+        // }
         // printing array element
-        for(int i : arr){
-            System.out.print(i+ " ");
+        // for(int i : arr){
+        //     System.out.print(i+ " ");
+        // }
+
+        // Taking String Input
+        
+        String input =  sc.nextLine();
+        String ar[]= input.split(",");
+
+        ArrayList<Integer>part = new ArrayList<>();
+        for(String token : ar){
+            int num = Integer.parseInt(token);
+            part.add(num);
+        }
+
+        for(int i : part){
+            System.out.print(i +" ");
         }
 
         sc.close();
-
     }
 }
