b6dd20a3a1f3d13e2eef213b6be67d34fb46a423
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-04-13 21:17:08 +0530

    revision

--- Patterns/pattern1.java
+++ Patterns/pattern1.java
@@ -32,7 +32,7 @@ public class pattern1 {
         //     System.out.println();
         // }
 
-        // Pattern 4
+        // // Pattern 4
         // int n = 5;
         // for(int row=1;row<=n; row++){
         //     for(int col=1; col<=    n-row+1; col++){
@@ -41,16 +41,137 @@ public class pattern1 {
         //     System.out.println();
         // }
 
-        //Pattern 5
+        // Pattern 5
+        // int n = 5;
+        // for(int row = 1; row<=n; row++){
+        //     for(int col=1; col<=n-row; col++){
+        //         System.out.print("  ");
+        //     }
+        //     for(int col=1; col<=2*row-1; col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Pattern 6
+        // int n = 5;
+        // for(int row=1; row<=n; row++){
+        //     for(int col=1; col<=row-1; col++){
+        //         System.out.print("  ");
+        //     }
+        //     for(int col=1; col<=2*n-2*row-1; col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Pattern 7
+        // int n = 5;
+        // for(int row=1; row<=n; row++){
+        //     for(int col=1; col<=n; col++){
+        //         if (row==1 || row==n){
+        //             System.out.print("* ");
+        //         }else{
+        //             if(col==1 || col==n){
+        //                 System.out.print("* ");
+        //             }else{
+        //                 System.out.print("  ");
+        //             }
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // Pattern 8
+        // int n = 5;
+        // for(int row=1; row<=n; row++){
+        //     if (row==1 || row==2 ||row==n){
+        //         for(int col=1; col<=row; col++){
+        //             System.out.print("* ");
+        //         }
+        //     }else{
+        //         // star
+        //         System.out.print("* ");
+        //         // space
+        //         for(int col=1; col<=(row-2); col++){
+        //             System.out.print("  ");
+        //         }
+        //         // star
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Pattern 9
+        // int n = 5;
+        // for(int row=1; row<= n; row++){
+        //     for(int col=1; col<=n-row; col++){
+        //             System.out.print("  ");
+        //         }
+        //     if(row==1||row==n){
+        //         for(int col=1; col<=2*row-1; col++){
+        //             System.out.print("* ");
+        //         }
+        //     }else{
+                
+        //         System.out.print("* ");
+
+        //         for(int col=1; col<=2*row-3; col++){  
+        //             System.out.print("  ");
+        //         }
+
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Pattern 10
         int n = 5;
-        for(int row=1; row<=n; row++){
+        for(int row=1; row<= n; row++){
             for(int col=1; col<=n-row; col++){
-                System.out.print(" ");
+                    System.out.print("  ");
+                }
+            if(row==1){
+                for(int col=1; col<=2*row-1; col++){
+                    System.out.print("* ");
+                }
+            }else{
+                // 1 star
+                System.out.print("* ");
+                //space
+                for(int col=1; col<=2*row-3; col++){  
+                    System.out.print("  ");
+                }
+                // 1 star
+                System.out.print("* ");
             }
-            for(int col=1; col<=n-)
+            
+            System.out.println();
         }
+        for(int row=1; row<=n-1; row++){
 
+            // part-1 (spaces)
+            for(int col=1; col<=row; col++){
+                System.out.print("  ");
+            }
+            //part-2 (star)
 
+            if(row==n-1){
+                // 1 start
+                System.out.print("*");
+            }else{
+                // 1 start
+                System.out.print("* ");
+                // spaces
+                for(int col=1; col<=2*(n-row)-3; col++){
+                    System.out.print("  ");
+                }
+                // 1 star
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
 
         
     }
