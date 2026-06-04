c6e23318d54a4f6cbe0cc29f29645247746426d2
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-06-04 21:57:46 +0530

    revision

--- print.java
+++ print.java
@@ -1,5 +1,7 @@
 public class print{
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.print("*\n* *\n* * *\n* * * *");
     }
 }
+// comment
